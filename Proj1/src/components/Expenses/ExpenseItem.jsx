ef938ceda72dfd9032edc3928149aca3dd620fb5
Souvik Das <103851921+souvik-wizard@users.noreply.github.com>
2023-06-18 12:51:02 +0530

    assign 1 of react tute

---
+++ Proj1/src/components/Expenses/ExpenseItem.jsx
@@ -0,0 +1,23 @@
+import './ExpenseItem.css';
+import Card from '../UI/Card';
+import ExpenseDate from './ExpenseDate';
+
+function ExpenseItem(props) {
+
+  function clickHandler() {
+    console.log('Clicked!');
+  }
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change</button>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
