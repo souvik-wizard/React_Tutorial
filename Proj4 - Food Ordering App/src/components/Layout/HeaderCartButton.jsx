eea0fa6dbd628589191a7a8192db3e8432d61609
Souvik Das <103851921+souvik-wizard@users.noreply.github.com>
2023-08-15 12:28:52 +0530

    finished

--- Proj4 - Food Ordering App/src/components/Layout/HeaderCartButton.jsx
+++ Proj4 - Food Ordering App/src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,34 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../../components/Cart/CartIcon';
 import CartContext from '../../store/cart-context';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 const HeaderCartButton = (props) => {
+    const [highlightedBtn, setHighlightedBtn] = useState(false);
+
     const cardCtx = useContext(CartContext);
     const numberOfCartItems = cardCtx.items.reduce((curNumber, item) => {
-        return curNumber + item.amount; 
+        return curNumber + item.amount;
     }, 0);
 
+    const butonClasses = `${classes.button} ${highlightedBtn ? classes.bump : ''}`
+
+    useEffect(() => {
+        if (cardCtx.items.length === 0) {
+            return;
+        }
+        setHighlightedBtn(true);
+        const cleanUp = setTimeout(() => {
+            setHighlightedBtn(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(cleanUp);
+        };
+    }, [cardCtx.items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={butonClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
